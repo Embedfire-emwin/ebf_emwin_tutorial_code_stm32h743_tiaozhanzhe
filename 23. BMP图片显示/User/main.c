494dd195cd66fdfc97055a49922bc159518bfee1
zzz_xxj <995007786@qq.com>
2020-02-19 22:18:28 +0800

    添加裸机中文字库，BMP图片显示仍然有问题

--- 23. BMP图片显示/User/main.c
+++ 23. BMP图片显示/User/main.c
@@ -172,7 +172,7 @@ static void LED_Task(void* parameter)
 {
 	while(1)
 	{
-    printf("%d\r\n", (int)GUI_ALLOC_GetNumUsedBytes());
+//    printf("%d\r\n", (int)GUI_ALLOC_GetNumUsedBytes());
 		LED3_TOGGLE;
 		vTaskDelay(1000);
 	}
@@ -225,7 +225,7 @@ static void BSP_Init(void)
   SCB_EnableDCache();
   Board_MPU_Config(0, MPU_Normal_WT, 0x20000000, MPU_REGION_SIZE_128KB);
   Board_MPU_Config(1, MPU_Normal_WT, 0x24000000, MPU_REGION_SIZE_512KB);
-  Board_MPU_Config(2, MPU_Normal_WT, 0xD0000000, MPU_REGION_SIZE_32MB);
+  Board_MPU_Config(2, MPU_Normal_NonCache, 0xD0000000, MPU_REGION_SIZE_32MB);
   /*emwin动态内存配置为WT，在使用Alpha混合和内存设备时需要手动清空D-Cache，但RGB565下清空操作无效*/
   Board_MPU_Config(3,MPU_Normal_WT,0xD1800000,MPU_REGION_SIZE_8MB);
 	/* CRC和emWin没有关系，只是他们为了库的保护而做的
